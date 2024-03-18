e5b292755e52029b30fe18bb5ac3e9a63cc27ab3
Paolo <paolo@kin.works>
2024-03-18 17:05:10 +0000

    fix clipboard copy function name

--- admin/js/bookster-cpsw-admin.js
+++ admin/js/bookster-cpsw-admin.js
@@ -6,11 +6,11 @@ booksterCPSWDomReady(() => {
 	let bookster_shortcode = document.getElementById('bookster-cpsw-shortcode');
 
 	if(bookster_shortcode != null) {
-		bookster_shortcode.addEventListener('click', booksterCPSWDomReady(bookster_shortcode));
-		bookster_shortcode.addEventListener('focus', booksterCPSWDomReady(bookster_shortcode));
+		bookster_shortcode.addEventListener('click', booksterCPSWToClipboard(bookster_shortcode));
+		bookster_shortcode.addEventListener('focus', booksterCPSWToClipboard(bookster_shortcode));
 	}
 
-	function booksterCPSWDomReady(item){ 
+	function booksterCPSWToClipboard(item){ 
 		navigator.clipboard.writeText(item.value).then(
 			() => {
 				alert('Shortcode copied to clipboard');
